964035276144bfad2b3510c0ba47f308d8cc504b
prabandkumar <praband10@gmail.com>
2026-01-05 20:15:02 +0530

    Initial SQL To-Do List

---
+++ Todo list.sql
@@ -0,0 +1,26 @@
+-- Create database
+CREATE DATABASE todo_app;
+USE todo_app;
+
+-- Create table
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_name VARCHAR(100) NOT NULL,
+    status1 VARCHAR(20) DEFAULT 'Pending',
+    created_at DATE);
+
+-- Insert sample tasks
+INSERT INTO tasks (task_name, created_at)
+VALUES
+('Learn SQL basics', CURDATE()),
+('Practice queries', CURDATE()),
+('Build To-Do List project', CURDATE());
+
+-- View tasks
+SELECT * FROM tasks;
+
+-- Mark task as completed
+UPDATE tasks SET status1 = 'Completed' WHERE id = 2;
+
+-- Delete task
+DELETE FROM tasks WHERE id = 3;
